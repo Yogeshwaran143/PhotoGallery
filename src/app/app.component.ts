5b135ea21f5889ba211366767eea3d4b93cf2e59
YYogeshwaran143 <yoger.name>
2021-11-18 08:38:57 +0000

    first messege

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  cols: number;
+  rows: number;
+  imageUrl: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angularapp';
+  tiles: Tile[] = [
+    { cols: 1, rows: 2, imageUrl: 'assets/jeff.webp' },
+    { cols: 1, rows: 2, imageUrl: 'assets/marissa.jpg' },
+    { cols: 2, rows: 4, imageUrl: 'assets/mark.jpg' },
+    { cols: 2, rows: 2, imageUrl: 'assets/satya.jpg' },
+    { cols: 2, rows: 2, imageUrl: 'assets/sundhar.webp' },
+    { cols: 2, rows: 2, imageUrl: 'assets/tim.jpg' },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
